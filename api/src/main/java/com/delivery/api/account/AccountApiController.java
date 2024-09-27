1f90df977823edaedb0d205a6194c4902e01a80b
Leekukju <ckzmfk19@kakao.com>
2024-09-27 11:19:47 +0900

    ObjectMapper & swagger 설정 클래스 적용

--- api/src/main/java/com/delivery/api/account/AccountApiController.java
+++ api/src/main/java/com/delivery/api/account/AccountApiController.java
@@ -1,11 +1,14 @@
 package com.delivery.api.account;
 
+import com.delivery.api.account.model.AccountMeResponse;
 import com.delivery.db.account.AccountEntity;
 import com.delivery.db.account.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,10 +17,13 @@ public class AccountApiController {
 
     private final AccountRepository accountRepository;
 
-    @GetMapping("")
-    public void save() {
-        var account = AccountEntity.builder().build();
+    @GetMapping("/me")
+    public AccountMeResponse me() {
 
-        accountRepository.save(account);
+        return AccountMeResponse.builder()
+                .name("홍길동")
+                .email("A@gamil.com")
+                .registeredAt(LocalDateTime.now())
+                .build();
     }
 }
