be269a60b9976b3a45a6a549cc920c2aaaa076fa
Leekukju <ckzmfk19@kakao.com>
2024-10-23 09:42:57 +0900

    Api Error Code 적용

--- api/src/main/java/com/delivery/api/account/AccountApiController.java
+++ api/src/main/java/com/delivery/api/account/AccountApiController.java
@@ -1,6 +1,7 @@
 package com.delivery.api.account;
 
 import com.delivery.api.account.model.AccountMeResponse;
+import com.delivery.api.common.api.Api;
 import com.delivery.db.account.AccountEntity;
 import com.delivery.db.account.AccountRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,12 +19,13 @@ public class AccountApiController {
     private final AccountRepository accountRepository;
 
     @GetMapping("/me")
-    public AccountMeResponse me() {
-
-        return AccountMeResponse.builder()
+    public Api<AccountMeResponse> me() {
+        var response = AccountMeResponse.builder()
                 .name("홍길동")
                 .email("A@gamil.com")
                 .registeredAt(LocalDateTime.now())
                 .build();
+
+        return Api.OK(response);
     }
 }
