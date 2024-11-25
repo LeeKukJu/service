a0445de5047d128beb4ed96eecfe3bc13a4948dc
Leekukju <ckzmfk19@kakao.com>
2024-11-25 13:25:58 +0900

    [스토어 도메인 개발] Store Menu 서비스 로직 개발

---
+++ api/src/main/java/com/delivery/api/domain/storemenu/converter/StoreMenuConverter.java
@@ -0,0 +1,48 @@
+package com.delivery.api.domain.storemenu.converter;
+
+import com.delivery.api.common.annotation.Converter;
+import com.delivery.api.common.error.ErrorCode;
+import com.delivery.api.common.exception.ApiException;
+import com.delivery.api.domain.storemenu.controller.model.StoreMenuRegisterRequest;
+import com.delivery.api.domain.storemenu.controller.model.StoreMenuResponse;
+import com.delivery.db.storemenu.StoreMenuEntity;
+
+import java.util.Optional;
+
+@Converter
+public class StoreMenuConverter {
+
+    public StoreMenuEntity toEntity(StoreMenuRegisterRequest request) {
+
+        return Optional.ofNullable(request)
+                .map(it -> {
+                    return StoreMenuEntity.builder()
+                            .storeId(request.getStoreId())
+                            .name(request.getName())
+                            .amount(request.getAmount())
+                            .thumbnailUrl(request.getThumbnailUrl())
+                            .build();
+                })
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
+    }
+
+    public StoreMenuResponse toResponse(
+            StoreMenuEntity storeMenuEntity
+    ) {
+        return Optional.ofNullable(storeMenuEntity)
+                .map(it -> {
+                    return StoreMenuResponse.builder()
+                            .id(it.getId())
+                            .storeId(it.getStoreId())
+                            .name(it.getName())
+                            .amount(it.getAmount())
+                            .status(it.getStatus())
+                            .thumbnailUrl(it.getThumbnailUrl())
+                            .likeCount(it.getLikeCount())
+                            .sequence(it.getSequence())
+                            .build();
+                })
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
+
+    }
+}
