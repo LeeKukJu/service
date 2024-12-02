efe0e772378dcc2a05bc76fd55245b5454766785
Leekukju <ckzmfk19@kakao.com>
2024-12-02 16:42:01 +0900

    [상품 주문 개발] User Order 서비스 로직 개발2 - 주문 상태 코드 enum 추가 - 주문 상세 조회 - 주문 목록 조회(전체, 진행중인 내역, 과거내역) - 주문 상태 변경

--- db/src/main/java/com/delivery/db/userorder/enums/UserOrderStatus.java
+++ db/src/main/java/com/delivery/db/userorder/enums/UserOrderStatus.java
@@ -5,6 +5,17 @@ public enum UserOrderStatus {
     REGISTERED("등록"),
 
     UNREGISTERED("해지"),
+
+    ORDER("주문"),
+
+    ACCEPT("주문 확인"),
+
+    COOKING("요리중"),
+
+    DELIVERY("배달중"),
+
+    RECEIVED("배달 완료"),
+
     ;
 
     UserOrderStatus(String description) {
